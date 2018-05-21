a5e074267570b5dc747c2dbec20640d80d5b71a8
jonrhall <5432730+jonrhall@users.noreply.github.com>
2018-05-21 00:54:45 -0700

    Added sensor template and get-instances method.

--- src/resources/actors.js
+++ src/resources/actors.js
@@ -12,8 +12,6 @@ const actorInterface = require('../core/resource-interface')({
   }),
   httpClient = require('../core/http-client');
 
-
-
 // Add a toggle timer action for the Actor resource, to support the route CPBi provides
 actorInterface.setTimer = async (actor, secs) => {
   return await httpClient.post(`/actor/${actor.id}/toggle/${secs}`);
--- src/resources/sensors.js
+++ src/resources/sensors.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const sensorInterface = require('../core/resource-interface')({
-  name: 'sensors',
-  socketEvents: [ 'SENSOR_UPDATE' ]
-});
+    name: 'sensors',
+    socketEvents: [ 'SENSOR_UPDATE' ],
+    template: {
+      name: 'NewSensor',
+      hide: false,
+      type: 'DummyTempSensor',
+      config: {
+        temp: 0
+      }
+    }
+  }),
+  httpClient = require('../core/http-client');
+
+// Add a get method for returning a list of detected and available sensors
+sensorInterface.getSensorInstances = async () => {
+  return Object.values((await httpClient.getSystemDump()).sensor_instances);
+};
 
 module.exports = sensorInterface;
