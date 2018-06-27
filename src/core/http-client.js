b67cae2552b9ef61730daa146cf4996ac92a9163
jonrhall <jonrhall@users.noreply.github.com>
2018-06-26 21:41:12 -0700

    Removed cache mechanism from HTTP client, as it was causing end-user problems.

--- src/core/http-client.js
+++ src/core/http-client.js
@@ -2,7 +2,6 @@
 
 const fetch = require('node-fetch');
 
-const HTTP_INTERVAL = global.SWILL_SDK_CONFIG.cacheInterval;
 const HTTP_ADDRESS = `${global.SWILL_SDK_CONFIG.server}:${global.SWILL_SDK_CONFIG.port}`;
 
 const options = {
@@ -10,30 +9,9 @@ const options = {
   headers: { 'Access-Control-Allow-Origin': '*' }
 };
 
-// Initialize an empty cache
-let cache = {};
-
 // Make an API request to CraftBeerPi
 async function makeRequest(route, method, options){
-  let cachedResponse,
-    response;
-
-  // POST and PUT methods invalidate the cache because they could mutate server data
-  // that causes race conditions like how getSystemDump() would return stale data before
-  // the current HTTP_INTERVAL expires.
-  if(method === 'POST' || method === 'PUT'){
-    cache = {};
-  }
-
-  // For now, only retrieve GET responses from the cache
-  if(method === 'GET'){
-    cachedResponse = getCachedResponse(route, method);
-  }
-
-  // If there's a valid cached response, return it rather than fetching a new response
-  if(cachedResponse){
-    return cachedResponse;
-  }
+  let response;
 
   // Make the actual request to the server, get back a stream of data,
   // turn the stream into JSON. There's a lot of assumptions in this statement
@@ -49,36 +27,7 @@ async function makeRequest(route, method, options){
       throw new Error(`Error retrieving ${route}: ${err}`);
   }
 
-  // Cache the response
-  setCachedResponse(response, route, method);
-
   return response;
-}
-
-// Attempt to get a cached response
-function getCachedResponse(route, method){
-  let key = `${route}//${method}`;
-
-  // If there is a cache hit and it's within the tolerated interval for returning cached responses,
-  // serve that response, otherwise return null.
-  return (cache[key] && (Date.now() - cache[key].timestamp) < HTTP_INTERVAL) ?
-    cache[key].response :
-    null;
-}
-
-// Set a cached response
-function setCachedResponse(response, route, method){
-  cache[`${route}//${method}`] = CachedResponse({ response, route });
-}
-
-// The cached response object
-function CachedResponse(data){
-  return {
-    response: data.response || {},
-    timestamp: Date.now(),
-    route: data.route,
-    method: data.method || 'GET'
-  };
 }
 
 // Expose basic HTTP CRUD operations, and a special system dump command.
