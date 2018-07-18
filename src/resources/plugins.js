ca4dc8b92d0b6c9a3f6c515711fbc5c4437c6a4f
jonrhall <jonrhall@users.noreply.github.com>
2018-07-18 00:07:43 -0700

    Refactored plugin resource, added update/delete methods.

--- src/resources/plugins.js
+++ src/resources/plugins.js
@@ -1,39 +1,56 @@
 const httpClient = require('../core/http-client');
-const socketClient = require('../core/socket-client');
+
 const onUpdateFns = [];
-const socketEvents = [
-  'ADDON_DOWNLOAD_STARTED',
-  'ADDON_DOWNLOADED'
-];
 
 // Return an array of plugins
 async function getPlugins(){
   const pluginList = await httpClient.get('/editor/list');
-  return Object.keys(pluginList).map(name => ({
-    name,
-    ...(pluginList[name])
-  }));
+  return Object.keys(pluginList).map(name => Object.assign({}, pluginList[name], { name }));
 }
 
 // Download and install a plugin
-async function installPlugin(pluginName){
-  await httpClient.post(`/editor/${pluginName}/download`);
+async function installPlugin(name){
+  await httpClient.post(`/editor/${name}/download`);
 
-  return { name: pluginName };
+  // CraftBeerPi can be inconsistent in when it updates its resources via websocket and when it
+  // updates its resources only via HTTP response. This function makes it so that a user can rely
+  // on the 'onUpdate' function to truly give them all the resource object updates they need.
+  onUpdateFns.forEach(fn => fn('INSTALL_PLUGIN', { name }));
+
+  return { name };
+}
+
+// Download and install a plugin
+async function updatePlugin(name){
+  await httpClient.post(`/editor/${name}/update`);
+
+  // CraftBeerPi can be inconsistent in when it updates its resources via websocket and when it
+  // updates its resources only via HTTP response. This function makes it so that a user can rely
+  // on the 'onUpdate' function to truly give them all the resource object updates they need.
+  onUpdateFns.forEach(fn => fn('UPDATE_PLUGIN', { name }));
+
+  return { name };
+}
+
+// Download and install a plugin
+async function deletePlugin(name){
+  await httpClient.delete(`/editor/${name}`);
+
+  // CraftBeerPi can be inconsistent in when it updates its resources via websocket and when it
+  // updates its resources only via HTTP response. This function makes it so that a user can rely
+  // on the 'onUpdate' function to truly give them all the resource object updates they need.
+  onUpdateFns.forEach(fn => fn('DELETE_PLUGIN', { name }));
+
+  return { name };
 }
 
 // Allow clients to register listeners for when the config object updates.
 const onUpdate = fn => onUpdateFns.push(fn);
 
-// For any of the registered socket events, register listeners and make sure
-// to update any consumers of changes to the data models in the socket updates.
-socketEvents.forEach(event => socketClient.on(
-  event,
-  data => onUpdateFns.forEach(fn => fn(event, data))
-));
-
 module.exports = {
   getPlugins,
   installPlugin,
+  updatePlugin,
+  deletePlugin,
   onUpdate
 };
